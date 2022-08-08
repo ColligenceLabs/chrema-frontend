3d7271a26f975a2dd7f3284f8c67d55e6803ae41
helloljho@gmail.com <helloljho@gmail.com>
2022-08-08 10:46:45 +0900

    add create item page

--- src/views/NftsMarket/CreateNewItem.jsx
+++ src/views/NftsMarket/CreateNewItem.jsx
@@ -1,17 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import MarketLayout from '../../layouts/market-layout/MarketLayout';
-import { Box, Container, MenuItem, Typography } from '@mui/material';
+import { Box, Button, Container, Menu, MenuItem, Paper, Typography } from '@mui/material';
 import { styled } from '@mui/material/styles';
 import ImageSelector from '../../components/ImageSelector/ImageSelector';
 import CustomTextField from '../../components/forms/custom-elements/CustomTextField';
 import CustomTextarea from '../../components/forms/custom-elements/CustomTextarea';
 import CustomSelect from '../../components/forms/custom-elements/CustomSelect';
+import { useNavigate } from 'react-router-dom';
 
 const CreateNewItemContainer = styled(Container)`
   max-width: 646px !important;
   display: flex;
   flex-direction: column;
   gap: 2rem;
+  margin-bottom: 10rem;
 `;
 
 const TitleWrapper = styled(Typography)`
@@ -43,67 +45,171 @@ const FiledTitle = styled('label')`
 `;
 
 const FieldSubscription = styled('span')`
+  display: flex;
+  justify-content: space-between;
   color: rgb(112, 122, 131);
   font-size: 12px;
   font-weight: 500;
 `;
 
+const CreateCollection = styled('div')(({ theme }) => ({
+  fontSize: '14px',
+  fontWeight: 600,
+  color: theme.palette.primary.main,
+  cursor: 'pointer',
+}));
+
+const StyledMenuItem = styled(MenuItem)`
+  padding: 10px;
+  font-size: 16px;
+  font-weight: 600;
+  border-bottom: 1px solid rgb(229, 232, 235);
+  &:last-child {
+    border-bottom: none;
+  }
+`;
+
+const categories = [
+  {
+    id: 0,
+    value: 'picture',
+    caption: 'Picture',
+  },
+  {
+    id: 1,
+    value: 'painting',
+    caption: 'Painting',
+  },
+  {
+    id: 2,
+    value: 'illustrate',
+    caption: 'Illustrate',
+  },
+  {
+    id: 3,
+    value: 'design',
+    caption: 'Design',
+  },
+  {
+    id: 4,
+    value: 'character',
+    caption: 'Character',
+  },
+  {
+    id: 5,
+    value: 'other',
+    caption: 'Other',
+  },
+];
+
 const CreateNewItem = () => {
+  const navigate = useNavigate();
+
+  const [nftItem, setNftItem] = useState(null);
+  const [collection, setCollection] = useState(null);
+  const [title, setTitle] = useState('');
+  const [description, setDescription] = useState('');
+  const [category, setCategory] = useState(0);
+  const [price, setPrice] = useState('');
+
+  const moveToPage = () => {
+    navigate('/market/mycollection');
+  };
+
   return (
     <MarketLayout>
       <CreateNewItemContainer>
         <TitleWrapper>Create New Item</TitleWrapper>
         <FieldWrapper>
-          <FiledTitle className="title" required={true}>
-            Image, Video, Audio
-          </FiledTitle>
-          <FieldSubscription className="subscription" variant="h6">
+          <FiledTitle required={true}>Image, Video, Audio</FiledTitle>
+          <FieldSubscription variant="h6">
             File type supported: JPG, PNG, GIF, MP4. Max size: 100 MB
           </FieldSubscription>
           <ImageSelector />
         </FieldWrapper>
 
         <FieldWrapper>
-          <FiledTitle className="title" required={true}>
-            Name
-          </FiledTitle>
-          <CustomTextField variant="outlined" fullWidth size="small" />
-        </FieldWrapper>
-
-        <FieldWrapper>
-          <FiledTitle className="title">External link</FiledTitle>
-          <FieldSubscription className="subscription" variant="h6">
-            NTal will include a link to this URL on this item&lsquo;s detail page, so that users can
-            click to learn more about it. You are welcome to link to your own webpage with more
-            details.
+          <FiledTitle>Collection</FiledTitle>
+          <FieldSubscription variant="h6">
+            This is the collection where your item will appear.{' '}
+            <CreateCollection onClick={moveToPage}>My Collection</CreateCollection>
           </FieldSubscription>
+          <CustomSelect
+            name="collection"
+            // value={values.level}
+            defaultValue="collection"
+            fullWidth
+            size="small"
+            PaperProps={{
+              elevation: 0,
+              sx: {
+                minWidth: '250px',
+                overflow: 'visible',
+                filter: 'drop-shadow(0px 2px 8px rgba(0,0,0,0.32))',
+                mt: 2.2,
+                '& .MuiMenu-list': {
+                  p: 0,
+                },
+              },
+            }}
+          >
+            <StyledMenuItem value="collection">Collection</StyledMenuItem>
+          </CustomSelect>
+        </FieldWrapper>
+
+        <FieldWrapper>
+          <FiledTitle required={true}>Name</FiledTitle>
           <CustomTextField variant="outlined" fullWidth size="small" />
         </FieldWrapper>
 
         <FieldWrapper>
-          <FiledTitle className="title">Description</FiledTitle>
-          <FieldSubscription className="subscription" variant="h6">
-            NTal will include a link to this URL on this item&lsquo;s detail page, so that users can
-            click to learn more about it. You are welcome to link to your own webpage with more
-            details.
+          <FiledTitle>Description</FiledTitle>
+          <FieldSubscription variant="h6">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Debitis, necessitatibus?
           </FieldSubscription>
           <CustomTextarea maxRows={5} minRows={5} />
         </FieldWrapper>
 
         <FieldWrapper>
-          <FiledTitle className="title">Collection</FiledTitle>
-          <FieldSubscription className="subscription" variant="h6">
-            This is the collection where your item will appear.
+          <FiledTitle>Category</FiledTitle>
+          <FieldSubscription variant="h6">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
           </FieldSubscription>
           <CustomSelect
-            name="level"
+            name="category"
             // value={values.level}
-            defaultValue="collection"
+            defaultValue="category"
             fullWidth
             size="small"
+            PaperProps={{
+              elevation: 0,
+              sx: {
+                minWidth: '250px',
+                overflow: 'visible',
+                filter: 'drop-shadow(0px 2px 8px rgba(0,0,0,0.32))',
+                mt: 2.2,
+                '& .MuiMenu-list': {
+                  p: 0,
+                },
+              },
+            }}
           >
-            <MenuItem value="collection">Collection</MenuItem>
+            {categories.map((category) => (
+              <StyledMenuItem key={category.id} value={category.value}>
+                {category.caption}
+              </StyledMenuItem>
+            ))}
           </CustomSelect>
+        </FieldWrapper>
+
+        <FieldWrapper>
+          <FiledTitle>Price</FiledTitle>
+          <FieldSubscription variant="h6">Lorem ipsum dolor sit amet.</FieldSubscription>
+          <CustomTextField variant="outlined" fullWidth size="small" />
+        </FieldWrapper>
+
+        <FieldWrapper>
+          <Button variant="contained">Create</Button>
         </FieldWrapper>
       </CreateNewItemContainer>
     </MarketLayout>
