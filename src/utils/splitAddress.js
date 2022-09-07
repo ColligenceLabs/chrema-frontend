043937acb9a5d5eee5a758b45343c8033a34922c
helloljho@gmail.com <helloljho@gmail.com>
2022-09-07 18:27:23 +0900

    change wallet info layout

--- src/utils/splitAddress.js
+++ src/utils/splitAddress.js
@@ -1,10 +1,10 @@
 import contracts from '../config/constants/contracts';
 
-const splitAddress = (str = '') => {
+const splitAddress = (str = '', length = 5) => {
   const contract = contracts.market[process.env.REACT_APP_MAINNET === 'true' ? 8217 : 1001];
   if (!str || str === '' || str === '0x0000000000000000000000000000000000000000') return '-';
   if (str === contract) return 'Market';
-  return str.substr(0, 5) + '...' + str.substr(str.length - 5, str.length);
+  return str.substr(0, length) + '...' + str.substr(str.length - length, str.length);
 };
 
 export default splitAddress;
