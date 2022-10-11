28bc6b9b03c8ee509ad801259d9ba1f7d1badb88
helloljho@gmail.com <helloljho@gmail.com>
2022-10-11 18:05:57 +0900

    hide my nft amount - rollback

--- src/views/NftsMarket/components/DetailComponents/DetailSell/DetailSell.tsx
+++ src/views/NftsMarket/components/DetailComponents/DetailSell/DetailSell.tsx
@@ -222,19 +222,18 @@ const DetailSell: React.FC<DetailSellProps> = ({
               gap: '1rem',
             }}
           >
-            {/*{data?.data?.collection_id?.contract_type === 'KIP37' && (*/}
-            {/*  <Box sx={{ flex: 2 }}>*/}
-            {/*    <Typography variant={'subtitle2'} color={'primary'}>*/}
-            {/*      Amount*/}
-            {/*    </Typography>*/}
+            {data?.data?.collection_id?.contract_type === 'KIP37' && (
+              <Box sx={{ flex: 2 }}>
+                <Typography variant={'subtitle2'} color={'primary'}>
+                  Amount
+                </Typography>
 
-            {/*    <Typography variant={'h3'}>{myNFTCount}</Typography>*/}
-            {/*   */}
-            {/*  </Box>*/}
-            {/*)}*/}
+                <Typography variant={'h3'}>{myNFTCount}</Typography>
+              </Box>
+            )}
             {data?.data?.seller ? (
               <>
-                <Box sx={{ flex: 2 }}>
+                <Box sx={{ flex: 1 }}>
                   <Typography variant={'subtitle2'} color={'primary'}>
                     {`Unit Price (${data?.data?.quote.toUpperCase()})`}
                   </Typography>
@@ -253,10 +252,10 @@ const DetailSell: React.FC<DetailSellProps> = ({
                         setSellPrice(event.target.value);
                       }
                     }}
-                    fullWidth
+                    // fullWidth
                   />
                 </Box>
-                <Box sx={{ flex: 1, width: smDown ? '50px' : '100px' }}>
+                <Box sx={{ flex: 1, width: smDown ? '50px' : '100px', border: '2px solid red' }}>
                   <LoadingButton
                     disabled={
                       totalPrice === 0 ||
@@ -279,7 +278,7 @@ const DetailSell: React.FC<DetailSellProps> = ({
                   <LoadingButton
                     loading={loading}
                     variant="contained"
-                    fullWidth
+                    // fullWidth
                     onClick={openSchedule}
                   >
                     Sell as Rental NFT
