f6356c0e11b68726788172f419ef31446caf966b
Peter H. Nahm <hwnahm@gmail.com>
2023-02-08 19:28:39 +0900

    hide redux on production

--- src/redux/Store.js
+++ src/redux/Store.js
@@ -3,11 +3,12 @@ import thunk from 'redux-thunk';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import RootReducers from './Rootreducers';
 
+const devTools =
+  process.env.NODE_ENV === 'production'
+    ? applyMiddleware(thunk)
+    : composeWithDevTools(applyMiddleware(thunk));
+
 export function configureStore(InitialState) {
-  const Store = createStore(
-    RootReducers,
-    InitialState,
-    composeWithDevTools(applyMiddleware(thunk)),
-  );
+  const Store = createStore(RootReducers, InitialState, devTools);
   return Store;
 }
